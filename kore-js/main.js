246445a5ba946847f77fc06daf3b8b2bbd274ff3
koribot <w.cawasa@gmail.com>
2026-03-06 18:49:08 +0800

    add workflow

--- kore-js/main.js
+++ kore-js/main.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'development' || process.env.KORJS_ENV === 'dev') {
-  module.exports = require('./dist/korjs.dev.js');
+  module.exports = require('./dist/kore-js.dev.js');
 } else {
-  module.exports = require('./dist/korjs.js');
+  module.exports = require('./dist/kore-js.js');
 }
